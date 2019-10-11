32e3765d7ac51a6d629a2a5676ada551ef4b191a
Marco Fracassi <fracassi.marco@gmail.com>
2019-10-11 11:59:20 +0200

    Add list route

--- src/main/kotlin/hack/Main.kt
+++ src/main/kotlin/hack/Main.kt
@@ -3,18 +3,30 @@ package hack
 import org.eclipse.jetty.http.HttpStatus
 import spark.Request
 import spark.Response
-import spark.Spark
+import spark.Spark.get
+import spark.Spark.port
+import com.eclipsesource.json.Json
+
+var offers = arrayOf("a", "b")
 
 fun main(){
-    Spark.port(8080)
+    port(8080)
+    get("/alive", aliveRoute())
+    get("/list", listRoute())
+}
 
-    Spark.get("/alive", aliveRoute())
+private fun listRoute(): (Request, Response) -> String {
+    return { _, res ->
+        res.type("application/json")
+        res.status(HttpStatus.OK_200)
+        Json.array(*offers).toString()
+    }
 }
 
 private fun aliveRoute(): (Request, Response) -> String {
     return { _, res ->
         res.type("application/json")
         res.status(HttpStatus.OK_200)
-        """{}"""
+        "Hello"
     }
 }
