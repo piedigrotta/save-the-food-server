1a86be610c46138c76bc24a7843b5b9120bbb258
Marco Fracassi <fracassi.marco@gmail.com>
2019-10-11 12:22:19 +0200

    add save route

--- src/main/kotlin/hack/Main.kt
+++ src/main/kotlin/hack/Main.kt
@@ -7,19 +7,27 @@ import spark.Spark.get
 import spark.Spark.port
 import com.eclipsesource.json.Json
 
-var offers = arrayOf("a", "b")
+var offers = mutableListOf("a", "b")
 
 fun main(){
     port(8080)
     get("/alive", aliveRoute())
     get("/list", listRoute())
+    get("/save", saveRoute())
 }
 
 private fun listRoute(): (Request, Response) -> String {
     return { _, res ->
         res.type("application/json")
         res.status(HttpStatus.OK_200)
-        Json.array(*offers).toString()
+        Json.array(*offers.toTypedArray()).toString()
+    }
+}
+
+private fun saveRoute(): (Request, Response) -> String {
+    return { _, res ->
+        offers.add("cane")
+        "ok"
     }
 }
 
