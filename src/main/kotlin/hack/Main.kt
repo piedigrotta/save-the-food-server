51aba1a4b532e811a95b9a05cf0861cbde79bfbc
Marco Fracassi <fracassi.marco@gmail.com>
2019-10-11 14:15:30 +0200

    add static site

--- src/main/kotlin/hack/Main.kt
+++ src/main/kotlin/hack/Main.kt
@@ -3,13 +3,13 @@ package hack
 import org.eclipse.jetty.http.HttpStatus
 import spark.Request
 import spark.Response
-import spark.Spark.get
-import spark.Spark.port
+import spark.Spark.*
 
 var offers = mutableListOf<Publisher>()
 var publishers = mutableListOf(Publisher(1, "Panetteria Fracassi", "Via Moruzzi 1, Pavia", "9:00-18:00", "Pane, pizze e focacce al 50%"))
 
 fun main() {
+    staticFiles.location("/public");
     port(8080)
     get("/alive", aliveRoute())
     get("/list", ListRoute())
